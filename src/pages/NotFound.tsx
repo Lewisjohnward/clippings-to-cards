d9b730a48edcbe57beabf99f8173a8ac485b7ac9
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-21 10:45:17 +0000

    Styled not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom";
 export const NotFound = () => {
   return (
-    <div>
-      <div>404 not found</div>
+    <div className="text-center [&>*]:pb-4">
+      <p className="text-4xl">404 not found</p>
+      <Link to="/" className="text-center ">
+        Return home
+      </Link>
     </div>
   );
 };
-
