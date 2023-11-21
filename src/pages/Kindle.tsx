b82b68e05f19d9fb142251b7758f5fff21a96cfc
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-21 10:44:48 +0000

    Add onDrop onDragOver logic

--- src/pages/Kindle.tsx
+++ src/pages/Kindle.tsx
@@ -1,9 +1,53 @@
 import { FcKindle } from "../misc/icons";
 
-export const Kindle = (props: {}) => {
+export const Kindle = () => {
+  const handleDragEnd = (event) => {
+    event.preventDefault();
+    console.log("Drag end");
+  };
+  const handleDrag = (event) => {
+    event.preventDefault();
+    console.log("Drag");
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    if (event.dataTransfer.items) {
+      console.log(event.dataTransfer.items);
+      console.log(event.dataTransfer.items[0].getAsFile());
+    }
+    //   if (ev.dataTransfer.items) {
+    //   // Use DataTransferItemList interface to access the file(s)
+    //   [...ev.dataTransfer.items].forEach((item, i) => {
+    //     // If dropped items aren't files, reject them
+    //     if (item.kind === "file") {
+    //       const file = item.getAsFile();
+    //       console.log(`… file[${i}].name = ${file.name}`);
+    //     }
+    //   });
+    // } else {
+    //   // Use DataTransfer interface to access the file(s)
+    //   [...ev.dataTransfer.files].forEach((file, i) => {
+    //     console.log(`… file[${i}].name = ${file.name}`);
+    //   });
+    // }
+    console.log("drop");
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    console.log("Drag over");
+  };
+
   return (
     <>
-      <div className="hidden md:flex justify-center items-center">
+      <div
+        className="hidden md:flex justify-center items-center"
+        onDrag={handleDrag}
+        onDragEnd={handleDragEnd}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+      >
         <FcKindle size={300} />
       </div>
       <div className="flex justify-center items-center w-full lg:w-1/4 md:w-1/2 h-full">
