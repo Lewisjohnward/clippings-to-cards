28c7a94f30e5fd9b96d812740ea66b43a6c8c95a
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-22 11:31:58 +0000

    Build grid to display books

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { dummyData } from "../misc/dummyData";
+import { Link } from "react-router-dom";
 
 export const Books = () => {
   const [books, setBooks] = useState(dummyData);
 
   return (
-    <div className="flex-grow h-full bg-blue-200 p-10">
-      <div className="bg-red-200">
+    <div className="flex-grow h-full w-full bg-blue-200 p-10">
+      <div className="space-y-2">
         <h2 className="text-xl">Books</h2>
+        <div className="grid grid-cols-4 gap-4 bg-red-200">
+          {books.map((book) => (
+            <div key={book.id} className="bg-sky-700 rounded">
+              <Link to={`/clippings/test`}>
+                <h3>{book.title}</h3>
+              </Link>
+              <p>{book.highlights.length}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
