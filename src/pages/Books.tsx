cab20cd2fdb15e27eaaa025c633126888d85a851
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-22 15:19:02 +0000

    hoist state to parent App

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import { dummyData } from "../misc/dummyData";
 import { Link } from "react-router-dom";
+import Clippings from "../types/clippings";
 
-export const Books = () => {
-  const [books, setBooks] = useState(dummyData);
-
+export const Books = ({ clippings }: { clippings: Clippings[] }) => {
   return (
     <div className="flex-grow h-full w-full bg-blue-200 p-10">
       <div className="space-y-2">
         <h2 className="text-xl">Books</h2>
-        <div className="grid grid-cols-4 gap-4 bg-red-200">
-          {books.map((book) => (
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 bg-red-200">
+          {clippings.map((book) => (
             <div key={book.id} className="bg-sky-700 rounded">
-              <Link to={`/clippings/test`}>
+              <Link to={`/clippings/${book.title}`}>
                 <h3>{book.title}</h3>
               </Link>
               <p>{book.highlights.length}</p>
