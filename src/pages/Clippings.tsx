e1217cb64f79195357d5df0d91750922135c7d59
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-22 15:20:04 +0000

    Use query params to get book title

--- src/pages/Clippings.tsx
+++ src/pages/Clippings.tsx
@@ -1,9 +1,11 @@
+import { useParams } from "react-router-dom";
 
 export const Clippings = (props: {}) => {
+  const { id } = useParams();
   return (
     <div>
       <div>clippings</div>
+      <div>{id}</div>
     </div>
   );
 };
-
