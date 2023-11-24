4ddd290a416ffd2c8d697312ef2b760684bc9eb3
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-24 16:23:34 +0000

    Create containing div

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 export const Home = () => {
   return (
-    <>
+    <div className="h-full flex justify-center items-center">
       <Link to="/kindle">kindle</Link>
-    </>
+    </div>
   );
 };
