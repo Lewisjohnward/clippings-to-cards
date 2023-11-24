4bea8cce8c2fcb46dc4a3c1e80d7d5efe77a4aeb
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-24 16:23:11 +0000

    Update style

--- src/pages/Clippings.tsx
+++ src/pages/Clippings.tsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export const Clippings = (props: {}) => {
   const { id } = useParams();
   return (
-    <div>
-      <div>clippings</div>
-      <div>{id}</div>
+    <div className="p-10 bg-sky-50">
+      <h2 className="text-xl">
+        <Link to="/books" className="underline">
+          Clippings
+        </Link>
+        {` > ${id}`}
+      </h2>
     </div>
   );
 };
