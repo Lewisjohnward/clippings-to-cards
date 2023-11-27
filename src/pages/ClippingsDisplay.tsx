44b2f21fa56ddde7a8e38fc040cd1e105886e73c
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-27 01:12:17 +0000

    Add bin/checkbox icon

--- src/pages/ClippingsDisplay.tsx
+++ src/pages/ClippingsDisplay.tsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import Clippings from "../types/clippings";
 import { clsx } from "clsx";
 import { MdDelete } from "../misc/icons";
+import { Checkbox, IconButton } from "@material-tailwind/react";
 
 interface Props {
   clippings: Clippings[];
@@ -12,12 +13,18 @@ export const ClippingsDisplay = ({ clippings }: Props) => {
   const [book] = clippings.filter((d) => d.title == id);
   return (
     <div className="p-10 bg-sky-50 space-y-2">
-      <h2 className="text-xl">
-        <Link to="/books" className="underline">
-          Books
-        </Link>
-        {` > ${id}`}
-      </h2>
+      <div className="flex justify-between items-center px-2 bg-white rounded">
+        <h2 className="text-xl">
+          <Link to="/books" className="underline">
+            Books
+          </Link>
+          {` > ${id}`}
+        </h2>
+        <Checkbox
+          ripple={false}
+          className="h-6 w-6 border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+        />
+      </div>
       <div className="space-y-4">
         {book.highlights.map((d, i) => {
           const number = i + 1;
@@ -25,7 +32,7 @@ export const ClippingsDisplay = ({ clippings }: Props) => {
             <div
               key={d.id}
               className={clsx(
-                "flex justify-between items-center gap-4 p-2",
+                "flex justify-between items-center gap-4 p-2 rounded shadow-lg",
                 i % 2 == 0 ? "bg-white" : "bg-yellow-200",
               )}
             >
@@ -33,9 +40,15 @@ export const ClippingsDisplay = ({ clippings }: Props) => {
                 <p>{number}</p>
                 <p>{d.text}</p>
               </div>
-              <button>
-                <MdDelete size={20} />
-              </button>
+              <div className="flex flex-col items-center justify-center">
+                <Checkbox
+                  ripple={false}
+                  className="h-6 w-6 border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+                />
+                <IconButton size="sm" variant="outlined">
+                  <MdDelete size={20} />
+                </IconButton>
+              </div>
             </div>
           );
         })}
