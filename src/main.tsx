32a3cc1db4b4f95155f4c0bb87484d262ef85dd1
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-27 01:11:54 +0000

    Add Material Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+import { ThemeProvider } from "@material-tailwind/react";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </React.StrictMode>
   </BrowserRouter>,
 );
