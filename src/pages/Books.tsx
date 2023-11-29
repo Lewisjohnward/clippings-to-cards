0ca5da5f22107a2fd08b9370410d75ce4b817833
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-11-29 16:53:18 +0000

    Add total counter

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -3,27 +3,30 @@ import Clippings from "../types/clippings";
 
 // Change component name to Clippings
 export const Books = ({ clippings }: { clippings: Clippings[] }) => {
+  const total = clippings.reduce(
+    (sum, clipping) => sum + clipping.highlights.length,
+    0,
+  ); // 17.5
+
+  // Able to extract function
+  const _better_total = clippings
+    .map((clipping) => clipping.highlights.length)
+    .reduce((sum, val) => sum + val, 0);
+  console.log(_better_total);
+
   return (
     <div className="flex-grow h-full w-full p-10">
       <div className="space-y-2">
-        <div className="flex gap-4">
-          {/* Change to site/clippings site/clippings/bookname site/clippings/all site/clippings/selected*/}
-          <h2 className="text-xl">Books</h2>
-          <Link to="/clippings/all" className="text-xl">
-            All
-          </Link>
-          <Link to="/clippings/selected" className="text-xl">
-            Selected
-          </Link>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4  bg-red-100">
+        {/* Change to site/clippings site/clippings/bookname site/clippings/all site/clippings/selected*/}
+        <h2 className="text-xl">Books</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4">
           <Link to={`/clippings/all`}>
             <div className="h-full flex flex-col justify-between p-4 space-y-2 bg-white rounded shadow-xl">
               <div className="space-y-1">
                 <h3>All</h3>
                 <p className="italic text-xs">-</p>
               </div>
-              <p className="text-right">500</p>
+              <p className="text-right">{total}</p>
             </div>
           </Link>
           <Link to={`/clippings/all`}>
