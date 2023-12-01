4da881247367bae6d8d8f20fa881d51373f8e1a2
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-01 18:11:02 +0000

    Get bookPosition/highlightPosition to update selected

--- src/stores/useBookStore.ts
+++ src/stores/useBookStore.ts
@@ -16,26 +16,22 @@ export const useBookStore = create<Store>()(
     immer((set, get) => ({
       books: [],
 
-      initialiseBooks: (books) =>
-        set(() => {
-          ({ books });
-        }),
+      initialiseBooks: (books) => set(() => ({ books })),
 
-      toggleHighlight: (bookName, position) => {
-        // Using highlight object from component find the bookPos and highlightPos
-        console.log(bookName);
-        const bookArr = get().books;
-        if (bookName === "all") {
-          // 9889ae22-e4f1-4cfb-bd8f-399b2fe877e7
-          console.log(bookArr);
-          // const pos = bookArr.findIndex((book) => book.highlights.includes());
-        }
-        const bookPosition = get()
-          .books.map((book) => book.title)
-          .indexOf(bookName);
+      toggleHighlight: (highlight) => {
+        const booksArray = get().books;
+
+        const bookPosition = booksArray
+          .map((book) => book.title)
+          .indexOf(highlight.title);
+
+        const highlightPosition = booksArray[bookPosition].highlights
+          .map((highlight) => highlight.id)
+          .indexOf(highlight.id);
+
         return set((state) => {
-          state.books[bookPosition].highlights[position].selected =
-            !state.books[bookPosition].highlights[position].selected;
+          state.books[bookPosition].highlights[highlightPosition].selected =
+            !state.books[bookPosition].highlights[highlightPosition].selected;
         });
       },
 
