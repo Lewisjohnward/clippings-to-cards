03f8d02d7e2a5c7a1c343308630bfec37d59b19b
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-01 16:30:26 +0000

    Create getHighlights helper

---
+++ src/helpers/getHighlights.ts
@@ -0,0 +1,23 @@
+import { Books, Highlights } from "../types/Books";
+
+export const getHighlights = (books: Books[], id: string) => {
+  console.log("getHighlightsId ", id);
+  if (id === "all") {
+    const highlights: Highlights[] = [];
+    books.forEach((book) => highlights.push(...book.highlights));
+    return highlights;
+  } else if (id === "selected") {
+    const highlights: Highlights[] = [];
+    books.forEach((book) => highlights.push(...book.highlights));
+    const highlightsSelected = highlights.filter(
+      (highlight) => highlight.selected == true,
+    );
+    return highlightsSelected;
+  } else {
+    const booksFiltered = books.filter((d) => d.title == id);
+    if (booksFiltered.length === 0) return;
+
+    const [{ highlights }] = books.filter((d) => d.title == id);
+    return highlights;
+  }
+};
