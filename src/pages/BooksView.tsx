5d68243260fccf2299ba39911a216e8b9db81e60
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-01 14:17:46 +0000

    Add books/getCount from Zustand

--- src/pages/BooksView.tsx
+++ src/pages/BooksView.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Books from "../types/Books";
+import { useBookStore } from "../stores/useBookStore";
 
 const views = [
   { id: "all", text: "All", display: "total" },
@@ -7,17 +7,9 @@ const views = [
 ];
 
 // Change component name to Books
-export const BooksView = ({ books }: { books: Books[] }) => {
-  const total = books.reduce(
-    (sum, clipping) => sum + clipping.highlights.length,
-    0,
-  ); // 17.5
-
-  // Able to extract function
-  const _better_total = books
-    .map((clipping) => clipping.highlights.length)
-    .reduce((sum, val) => sum + val, 0);
-  console.log(_better_total);
+export const BooksView = () => {
+  const books = useBookStore((state) => state.books);
+  const getCount = useBookStore((state) => state.getCount);
 
   return (
     <div className="flex-grow h-full w-full p-10">
@@ -25,16 +17,15 @@ export const BooksView = ({ books }: { books: Books[] }) => {
         <h2 className="text-xl">Books</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4">
           {views.map((view) => {
-            const count = view.display == "total" ? total : 0;
             return (
               <Link
-                id={view.id}
+                key={view.id}
                 to={`/books/${view.id}`}
                 className="bg-white rounded shadow-xl"
               >
                 <div className="flex flex-col lg:flex-row justify-between p-4">
                   <h3>{view.text}</h3>
-                  <p className="text-left">{count} clippings</p>
+                  <p className="text-left">{getCount(view.id)} clippings</p>
                 </div>
               </Link>
             );
