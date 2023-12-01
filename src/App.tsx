9e542587ae5e14985e5d402ae26f075cda883470
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-01 14:17:06 +0000

    Remove passing props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { BooksView, Kindle, ClippingsView, NotFound } from "./pages/index";
 import { Home } from "./pages/Home";
 import { Layout } from "./components/Layout";
-import Books from "./types/Books";
 
 function App() {
-  const [books, setBooks] = useState<Books[]>([]);
-
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/kindle" element={<Kindle setBooks={setBooks} />} />
-        <Route path="/books" element={<BooksView books={books} />} />
-        <Route path="/books/:id" element={<ClippingsView books={books} />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route path="/kindle" element={<Kindle />} />
+        <Route path="/books" element={<BooksView />} />
+        <Route path="/books/:id" element={<ClippingsView />} />
       </Routes>
     </Layout>
   );
 }
+// <Route path="/*" element={<NotFound />} />
 
 export default App;
 // <div className="flex flex-col items-center justify-center">
