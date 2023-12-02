ef65f2cedfbdce7862930107f1fc8b641a7fd601
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-02 14:27:16 +0000

    Remove select all

--- src/pages/ClippingsView.tsx
+++ src/pages/ClippingsView.tsx
@@ -1,5 +1,4 @@
 import { Link, useParams } from "react-router-dom";
-import { Checkbox } from "@material-tailwind/react";
 import Clipping from "../components/Clipping";
 import { useBookStore } from "../stores/useBookStore";
 import { getHighlights } from "../helpers/getHighlights";
@@ -12,19 +11,14 @@ export const ClippingsView = () => {
   if (highlights === undefined) return <NoBooksFound />;
 
   return (
-    <div className="p-10 bg-sky-50 space-y-2">
-      <div className="flex justify-between items-center px-2 bg-white rounded shadow-lg">
+    <div className="bg-sky-50">
+      <div className="flex justify-between items-center p-4 bg-white shadow-lg">
         <h2 className="text-xl">
           <Link to="/books" className="underline">
             Books
           </Link>
           {` > ${bookName}`}
         </h2>
-        <Checkbox
-          ripple={false}
-          className="h-6 w-6 border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
-          crossOrigin={undefined}
-        />
       </div>
       <div className="rounded overflow-hidden">
         {highlights.map((highlight, position) => {
