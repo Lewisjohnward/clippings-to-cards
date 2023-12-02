6d43b8fa4c4d07b5f3f55deaddeac9a59dece774
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-02 16:37:54 +0000

    Change header style

--- src/pages/BooksView.tsx
+++ src/pages/BooksView.tsx
@@ -13,36 +13,38 @@ export const BooksView = () => {
 
   return (
     <div className="flex-grow h-full w-full p-10">
-      <div className="space-y-2">
-        <h2 className="text-xl">Books</h2>
-        <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4">
-          {views.map((view) => {
-            return (
-              <Link
-                key={view.id}
-                to={`/books/${view.id}`}
-                className="bg-white rounded shadow-xl"
-              >
-                <div className="flex flex-col lg:flex-row justify-between p-4">
-                  <h3>{view.text}</h3>
-                  <p className="text-left">{getCount(view.id)} clippings</p>
+      <div className="space-y-8">
+        <h2 className="text-4xl">Books</h2>
+        <div className="space-y-2">
+          <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4">
+            {views.map((view) => {
+              return (
+                <Link
+                  key={view.id}
+                  to={`/books/${view.id}`}
+                  className="bg-white rounded shadow-xl"
+                >
+                  <div className="flex flex-col lg:flex-row justify-between p-4">
+                    <h3>{view.text}</h3>
+                    <p className="text-left">{getCount(view.id)} clippings</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4">
+            {books.map((book) => (
+              <Link key={book.id} to={`/books/${book.title}`}>
+                <div className="h-full flex flex-col justify-between p-4 space-y-2 bg-white rounded shadow-xl">
+                  <div className="space-y-1">
+                    <h3>{book.title}</h3>
+                    <p className="italic text-xs">-{book.author}</p>
+                  </div>
+                  <p className="text-left">{`${book.highlights.length} clippings`}</p>
                 </div>
               </Link>
-            );
-          })}
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4">
-          {books.map((book) => (
-            <Link key={book.id} to={`/books/${book.title}`}>
-              <div className="h-full flex flex-col justify-between p-4 space-y-2 bg-white rounded shadow-xl">
-                <div className="space-y-1">
-                  <h3>{book.title}</h3>
-                  <p className="italic text-xs">-{book.author}</p>
-                </div>
-                <p className="text-left">{`${book.highlights.length} clippings`}</p>
-              </div>
-            </Link>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
