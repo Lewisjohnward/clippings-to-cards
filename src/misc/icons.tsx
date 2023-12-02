1cf100315a7b6ca51edf13bd4cf20fde9302e79d
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-02 16:17:31 +0000

    Add download icon

--- src/misc/icons.tsx
+++ src/misc/icons.tsx
@@ -1,5 +1,6 @@
 import { TbCardsFilled } from "react-icons/tb";
 import { FcKindle } from "react-icons/fc";
 import { MdDelete } from "react-icons/md";
+import { FaDownload } from "react-icons/fa6";
 
-export { TbCardsFilled, FcKindle, MdDelete };
+export { TbCardsFilled, FcKindle, MdDelete, FaDownload };
