5ae16e29c833ab017e918f8ecece853bddbeb053
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-03 18:52:04 +0000

    Add calender icon

--- src/misc/icons.tsx
+++ src/misc/icons.tsx
@@ -2,5 +2,6 @@ import { TbCardsFilled } from "react-icons/tb";
 import { FcKindle } from "react-icons/fc";
 import { MdDelete } from "react-icons/md";
 import { FaDownload } from "react-icons/fa6";
+import { SlCalender } from "react-icons/sl";
 
-export { TbCardsFilled, FcKindle, MdDelete, FaDownload };
+export { TbCardsFilled, FcKindle, MdDelete, FaDownload, SlCalender };
