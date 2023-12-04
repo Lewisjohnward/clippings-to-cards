1d779914d0aa98898d136a4eb19b68c321a520f5
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-04 01:09:07 +0000

    Add sort icons

--- src/misc/icons.tsx
+++ src/misc/icons.tsx
@@ -3,5 +3,15 @@ import { FcKindle } from "react-icons/fc";
 import { MdDelete } from "react-icons/md";
 import { FaDownload } from "react-icons/fa6";
 import { SlCalender } from "react-icons/sl";
+import { FaSortUp, FaSortDown } from "react-icons/fa6";
 
-export { TbCardsFilled, FcKindle, MdDelete, FaDownload, SlCalender };
+export {
+  TbCardsFilled,
+  FcKindle,
+  MdDelete,
+  FaDownload,
+  SlCalender,
+  FaSortDown,
+  FaSortUp,
+};
+
