3b222fb266fe00494bc7d626f4c5498f6b0c3815
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-05 19:36:43 +0000

    Display clippings in table

--- src/components/Clipping.tsx
+++ src/components/Clipping.tsx
@@ -10,7 +10,6 @@ export default function Clipping({
 }: {
   highlight: Highlights;
   position: number;
-  bookName: string;
 }) {
   const toggleSelected = useBookStore((state) => state.toggleSelected);
   const deleteHighlight = useBookStore((state) => state.deleteHighlight);
@@ -22,74 +21,35 @@ export default function Clipping({
   };
 
   return (
-    <div
+    <tr
       className={clsx(
-        "flex items-center gap-4 p-2",
+        "",
         (position + 1) % 2 == 0 ? "bg-white" : "bg-yellow-200",
       )}
     >
-      <p className="text-xs min-w-[20px] text-center">{position + 1}</p>
-      <div>
-        <p className="text-xs text-center opacity-60 italic">{"04/12/2023"}</p>
-        <p className="text-xs text-center opacity-60 italic">{"17:50:10"}</p>
-      </div>
-      <p className="text-xs basis-auto text-center opacity-60 italic">{5}</p>
-      <p className="text-sm">{highlight.text}</p>
-    </div>
+      <td className="p-4 text-xs text-center italic">{position + 1}</td>
+      <td className="text-xs text-center italic">
+        <div>
+          <p>{"04/12/2023"}</p>
+          <p>{"17:52:05"}</p>
+        </div>
+      </td>
+      <td className="text-xs text-center italic">{5}</td>
+      <td className="text-sm p-2">{highlight.text}</td>
+      <td>
+        <Checkbox
+          checked={highlight.selected}
+          ripple={false}
+          onChange={handleSelect}
+          className="h-6 w-6 border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+          crossOrigin={undefined}
+        />
+      </td>
+      <td>
+        <IconButton size="sm" variant="outlined">
+          <MdDelete size={20} onClick={handleDeleteHighlight} />
+        </IconButton>
+      </td>
+    </tr>
   );
 }
-
-    //
-    //       <label>Select all</label>
-    //       <Checkbox
-    //         checked={false}
-    //         ripple={false}
-    //         onChange={() => console.log("I need plugging in")}
-    //         className="h-6 w-6 border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
-    //         crossOrigin={undefined}
-    //       />
-    //       {bookName === "selected" && highlights.length > 0 && (
-    //         <Selected highlights={highlights} />
-    //       )}
-    //
-    //     <p className="bg-red-100">{position + 1}</p>
-    //     <div className="flex items-center gap-4 md:gap-8">
-    //       <div className="flex flex-col items-center bg-blue-500">
-    //         <p className="text-center opacity-60 italic">{"12/4/2023"}</p>
-    //         <p>{"17:20:15"}</p>
-    //       </div>
-    //       <p className="bg-blue-500 text-center opacity-60 italic">{5}</p>
-    //       </div>
-    //
-    //             <div className="flex items-center justify-center">
-    //     <Checkbox
-    //       checked={highlight.selected}
-    //       ripple={false}
-    //       onChange={handleSelect}
-    //       className="h-6 w-6 border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
-    //       crossOrigin={undefined}
-    //     />
-    //     <IconButton size="sm" variant="outlined">
-    //       <MdDelete size={20} onClick={handleDeleteHighlight} />
-    //     </IconButton>
-    //   </div>
-    //
-    //
-    //     <div>
-    //       <p className="text-xs text-center opacity-60 italic">
-    //         {"04/12/2023"}
-    //       </p>
-    //       <p className="text-xs text-center opacity-60 italic">{"17:50:10"}</p>
-    //     </div>
-    //     <p className="text-xs basis-auto text-center opacity-60 italic">{5}</p>
-    //
-    //     <p className="text-sm">{highlight.text}</p>
-    //     "flex justify-between items-center gap-2 md:gap-4 px-2 md:px-6 py-2",
-    //   <div className="grid grid-cols-2">
-    //     <p className="text-xs bg-red-400">{position + 1}</p>
-    //     <p className="text-xs text-center opacity-60 italic">{"04/12/2023"}</p>
-    //     <p className="text-xs text-center opacity-60 italic">{"17:50:10"}</p>
-    //   </div>
-    //   <p className="text-sm">{highlight.text}</p>
-    // </div>
-
