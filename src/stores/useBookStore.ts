5cb99be93934d96dd81129ca28098f1a506e81d1
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-07 11:38:04 +0000

    Remove date/toggleDate

--- src/stores/useBookStore.ts
+++ src/stores/useBookStore.ts
@@ -6,14 +6,12 @@ import SuperJSON from "superjson";
 
 type Store = {
   books: Books[];
-  displayDate: boolean;
-  toggleDisplayDate: () => void;
   initialiseBooks: (books: Books[]) => void;
   toggleSelected: (highlight: Highlights) => void;
   toggleSelectAll: (bookName: string) => void;
   deleteHighlight: (highlight: Highlights) => void;
   getCount: (selector: string) => number;
-  getHighlights: (id: string) => Highlights[] | undefined;
+  getHighlights: (id: string) => Highlights[];
 };
 
 const allSelected = (highlights: Highlights[]) => {
@@ -36,13 +34,6 @@ export const useBookStore = create<Store>()(
   persist(
     immer((set, get) => ({
       books: [],
-      displayDate: false,
-
-      toggleDisplayDate: () => {
-        set((state) => {
-          state.displayDate = !state.displayDate;
-        });
-      },
 
       initialiseBooks: (books) => set(() => ({ books })),
 
@@ -126,7 +117,7 @@ export const useBookStore = create<Store>()(
             .filter((highlight) => highlight.selected === true);
         } else {
           const booksFiltered = booksArr.filter((d) => d.title == id);
-          if (booksFiltered.length === 0) return;
+          if (booksFiltered.length === 0) return [];
 
           const [{ highlights }] = booksArr.filter((d) => d.title == id);
           return highlights;
