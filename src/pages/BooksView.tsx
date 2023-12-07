f5023ae2a1f4dcdae4a258bbf76d5981bda1c99f
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-07 14:13:17 +0000

    Disable selected when 0 selected clippings

--- src/pages/BooksView.tsx
+++ src/pages/BooksView.tsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
 import { useBookStore } from "../stores/useBookStore";
+import clsx from "clsx";
 
 const views = [
-  { id: "all", text: "All", display: "total" },
-  { id: "selected", text: "Selected", display: "selected" },
+  {
+    id: "all",
+    text: "All",
+  },
+  {
+    id: "selected",
+    text: "Selected",
+    style: "pointer-events-none text-opacity-20",
+  },
 ];
 
 // Change component name to Books
@@ -30,15 +38,19 @@ export const BooksView = () => {
         <div className="space-y-2">
           <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-4">
             {views.map((view) => {
+              const count = getCount(view.id);
               return (
                 <Link
                   key={view.id}
                   to={`/books/${view.id}`}
-                  className="bg-white rounded shadow-xl"
+                  className={clsx(
+                    "bg-white rounded shadow-xl text-black",
+                    view.id == "selected" && count == 0 && view.style,
+                  )}
                 >
                   <div className="flex flex-col lg:flex-row justify-between p-4">
                     <h3>{view.text}</h3>
-                    <p className="text-left">{getCount(view.id)} clippings</p>
+                    <p className="text-left">{count} clippings</p>
                   </div>
                 </Link>
               );
