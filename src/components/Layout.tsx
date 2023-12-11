d8d0db6a9f7cce3b1f2ddaa2b764b8e345e41448
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-11 10:03:25 +0000

    Deactivate books link when no books

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,12 @@
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
+import clsx from "clsx";
 import { TbCardsFilled } from "../misc/icons";
 import { Footer } from "./Footer";
+import { useBookStore } from "../stores/useBookStore";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+  const books = useBookStore((state) => state.books);
   return (
     <div className="flex flex-col gap-5 h-[100dvh] bg-amber-300 px-1 py-4 md:px-10 md:py-5">
       <div className="flex justify-between items-center gap-4">
@@ -12,7 +15,14 @@ export const Layout = ({ children }: { children: ReactNode }) => {
           <TbCardsFilled size={30} />
         </Link>
         <div className="flex gap-4 items-center underline">
-          <Link to="/books">Books</Link>
+          <Link
+            to="/books"
+            className={clsx(
+              books.length === 0 && "opacity-20 pointer-events-none",
+            )}
+          >
+            Books
+          </Link>
           <Link to="/kindle">Upload clippings</Link>
         </div>
       </div>
