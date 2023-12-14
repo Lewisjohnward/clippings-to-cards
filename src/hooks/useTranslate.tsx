69ae13673d3940933ece3e76c016f258ab945767
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-14 14:08:45 +0000

    Create translation object

--- src/hooks/useTranslate.tsx
+++ src/hooks/useTranslate.tsx
@@ -1,42 +1,60 @@
+import { MouseEvent } from "react";
 import { useBookStore } from "../stores/useBookStore";
+import { Highlights, Translation } from "../types/Books";
+import { v4 as uuidv4 } from "uuid";
 
 const LANGUAGE = `it`;
+
+interface Definition {
+  pos: string;
+  tr: {
+    fr: number;
+    pos: string;
+    text: string;
+  }[];
+}
 
 export const useTranslate = (highlight: Highlights) => {
   const appendTranslation = useBookStore(
     (state) => state.actions.appendTranslation,
   );
-  const translate = (e) => {
-    const word = e.target.innerText;
+  const translate = (e: MouseEvent<HTMLElement>) => {
+    const word = (e.target as HTMLElement).innerText;
 
     const str = `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?=&flags=4&key=dict.1.1.20231202T165200Z.f37a0db6c660a327.38c38e5f1732bcdf6faea905077ad49744d61e3d&lang=${LANGUAGE}-en&text=${word}`;
+
     fetch(str)
       .then((res) => res.json())
       .then((data) => {
-        let translationObj = [];
-        data.def.forEach((definition) => {
-          const str = [];
-          const originalWord = definition.text;
-          const type = definition.pos;
-          str.push(originalWord);
-          str.push(`(${type})`);
-          str.push("--");
+        const translation: Translation = {
+          id: uuidv4(),
+          word,
+          type: "?",
+          translation: [],
+        };
+        console.log(data);
 
-          definition.tr.forEach((translation: { text: any }) => {
-            console.log(translation.text);
-            const trans = translation.text;
-            str.push(trans);
+        if (data.def.length != 0) {
+          data.def.forEach((definition: Definition) => {
+            const translation: Translation = {
+              id: uuidv4(),
+              word,
+              type: "?",
+              translation: [],
+            };
+            translation.type = definition.pos;
+
+            definition.tr.forEach((tr) => {
+              translation.translation.push(tr.text);
+            });
+            appendTranslation(highlight, translation);
           });
-          translationObj.push(str);
-        });
-        if (translationObj.length == 0)
-          console.log("unable to find translation");
-        const translationArr = translationObj.map((translation) =>
-          translation.join(" "),
-        );
-        translationArr.forEach((translation) => {
+        } else {
+          translation.translation.length == 0 &&
+            translation.translation.push("unable to find translation");
+
           appendTranslation(highlight, translation);
-        });
+        }
       })
       .catch((error) => console.log(error));
   };
