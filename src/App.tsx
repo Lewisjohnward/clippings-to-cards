0dfc711a2f0de3fefadb1d5912285805da845038
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-15 13:19:32 +0000

    Change Kindle to Upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import { BooksView, Kindle, ClippingsView, NotFound } from "./pages/index";
+import { BooksView, ClippingsView, Upload, NotFound } from "./pages/index";
 import { Home } from "./pages/Home";
 import { Layout } from "./components/Layout";
 
 function App() {
   return (
-      <Layout>
-    <Routes>
+    <Layout>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/kindle" element={<Kindle />} />
-      <Route path="/books" element={<BooksView />} />
-      <Route path="/books/:id" element={<ClippingsView />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
-      </Layout>
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/books" element={<BooksView />} />
+        <Route path="/books/:id" element={<ClippingsView />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 }
 
