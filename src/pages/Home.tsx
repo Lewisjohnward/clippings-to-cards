0fce6c26642ae814edecb125f72166fae9145aed
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-15 13:21:08 +0000

    Change Kindle to Upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div className="h-full flex flex-col justify-center items-center space-y-4">
       <p>Convert your Kindle clippings to Anki flashcards</p>
-      <Link to="/kindle" className="text-4xl underline">
+      <Link to="/upload" className="text-4xl underline">
         Upload clippings
       </Link>
       <FcKindle className="text-[300px] md:text-[500px] textselect-none caret-transparent" />
