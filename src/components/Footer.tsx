0462478081b48bf4aa59099aeaed75d686851996
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-16 00:22:11 +0000

    Alter py

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="bg-yellow-400 py-4">
+    <div className="bg-yellow-400 py-2">
       <p className="text-xs italic text-center">Clippings to Cards, 2023</p>
     </div>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
   const books = useBooks();
   return (
     <div className="flex flex-col h-[100dvh]">
-      <div className="flex bg-yellow-400 justify-between items-center gap-4 px-2 py-4 md:px-10 md:py-5">
+      <div className="flex bg-yellow-400 justify-between items-center gap-4 px-2 py-2 md:px-10">
         <Link to="/" className="flex gap-4">
           <h1 className="text-bold text-xl">Clippings to Cards</h1>
           <TbCardsFilled size={30} />
