7f27c1684ddc5006045e23bbd1ae582b216a6580
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-16 01:22:06 +0000

    Extract modal component

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -3,6 +3,7 @@ import { FcKindle } from "../misc/icons";
 import { ChangeEvent, DragEvent } from "react";
 import { useUpload } from "../hooks/useUpload";
 import clsx from "clsx";
+import { Modal } from "../components/Modal";
 
 export const Upload = () => {
   const { modal, events, dragOver, proceedWithClippings, cancelClippings } =
@@ -14,28 +15,7 @@ export const Upload = () => {
         {/* Confirm modal */}
         {/* Error modal*/}
         {false && <Loading />}
-        <ReactModal
-          isOpen={modal.display}
-          appElement={document.getElementById("root") || undefined}
-          className="text-xl md:w-1/2 xl:w-2/6 p-4 space-y-4 bg-white rounded outline-none shadow-lg"
-          overlayClassName="absolute top-0 h-[100dvh] w-screen flex justify-center items-center p-4 bg-black bg-opacity-40"
-        >
-          <p>{modal.message}</p>
-          <div className="flex justify-center gap-4">
-            <button
-              onClick={modal.acknowledge}
-              className="bg-green-400 rounded px-4 py-2 text-white"
-            >
-              Okay
-            </button>
-            <button
-              onClick={cancelClippings}
-              className="bg-red-400 rounded px-4 py-2 text-white"
-            >
-              No
-            </button>
-          </div>
-        </ReactModal>
+        <Modal modal={modal} />
         <CardDropArea {...events} dragOver={dragOver} />
       </div>
     </div>
