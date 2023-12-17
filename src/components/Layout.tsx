02e7e7a60eedaea9a5d316e686cb46bb8dbfe9fe
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-17 13:10:17 +0000

    Add site icon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { TbCardsFilled } from "../misc/icons";
+import { NavLink } from "react-router-dom";
+import { SiteIcon } from "../misc/icons";
 import { Footer } from "./Footer";
 import { useBooks } from "../stores/useBookStore";
 
@@ -12,10 +12,17 @@ export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col h-[100dvh]">
       <div className="flex bg-yellow-400 justify-between items-center gap-4 px-2 py-2 md:px-10">
-        <Link to="/" className="flex gap-4">
+        <NavLink
+          to="/"
+          className={({ isActive }) => {
+            const baseStyle = "flex gap-2";
+            if (isActive) return `${baseStyle} pointer-events-none`;
+            else return `${baseStyle} hover:opacity-40`;
+          }}
+        >
+          <SiteIcon />
           <h1 className="text-bold text-xl">Clippings to Cards</h1>
-          <TbCardsFilled size={30} />
-        </Link>
+        </NavLink>
         <div className="flex gap-4 items-center">
           <NavLink
             to="/books"
