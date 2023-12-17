5888009d0a222c4c51691a295fc4ef052530684f
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-17 13:09:25 +0000

    Add website logo

--- src/misc/icons.tsx
+++ src/misc/icons.tsx
@@ -7,6 +7,18 @@ import { FaSortUp, FaSortDown } from "react-icons/fa6";
 import { BiSortAlt2 } from "react-icons/bi";
 import { MdOutlineRemoveCircle } from "react-icons/md";
 
+const SiteIcon = () => {
+  return (
+    <svg viewBox="0 0 72 28" width="50" height="28">
+      <g fill="#333">
+        <rect x="0" y="0" width="20" height="28" rx="4" ry="4"></rect>
+        <rect x="24" y="0" width="20" height="28" rx="4" ry="4"></rect>
+        <rect x="48" y="0" width="20" height="28" rx="4" ry="4"></rect>
+      </g>
+    </svg>
+  );
+};
+
 export {
   TbCardsFilled,
   BiSortAlt2,
@@ -16,6 +28,6 @@ export {
   SlCalender,
   FaSortDown,
   FaSortUp,
-  MdOutlineRemoveCircle
+  MdOutlineRemoveCircle,
+  SiteIcon,
 };
-
