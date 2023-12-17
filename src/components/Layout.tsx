822a8284c88a779aeaffba816355d0223e2e4b7a
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-17 11:48:49 +0000

    Change button style when inactive / no books

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { TbCardsFilled } from "../misc/icons";
 import { Footer } from "./Footer";
 import { useBooks } from "../stores/useBookStore";
 
-const disabledLink = "opacity-20 pointer-events-none";
+const disabledLink = "opacity-20 pointer-events-none underline";
 const enabledLink = "hover:opacity-40";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
@@ -16,12 +16,12 @@ export const Layout = ({ children }: { children: ReactNode }) => {
           <h1 className="text-bold text-xl">Clippings to Cards</h1>
           <TbCardsFilled size={30} />
         </Link>
-        <div className="flex gap-4 items-center underline">
+        <div className="flex gap-4 items-center">
           <NavLink
             to="/books"
             className={({ isActive }) => {
               if (isActive) return disabledLink;
-              if (books.length === 0) return disabledLink;
+              if (books.length === 0) return "opacity-20 pointer-events-none";
               return enabledLink;
             }}
           >
