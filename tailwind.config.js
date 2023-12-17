afa1d5bbc776dd2563cbb926baa1b380ea1247e4
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-17 13:08:01 +0000

    Add breakpoint for 1800px display

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-import  withMT  from "@material-tailwind/react/utils/withMT";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 /** @type {import('tailwindcss').Config} */
 export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1800px",
+      },
+    },
   },
   plugins: [],
 });
