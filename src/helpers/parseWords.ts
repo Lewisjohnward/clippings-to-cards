8f66e54547a9e8b6dbd553b060ce4870fc1a16e4
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-20 10:52:19 +0000

    Add removePunctuation function

--- src/helpers/parseWords.ts
+++ src/helpers/parseWords.ts
@@ -1,17 +1,20 @@
-export const getWords = (highlight: string) => {
-  const split = highlight
-    // .replace(/l'/g, "")
+export const removePunctuation = (word: string) => {
+  return word
     .replace(/m’/gi, "")
     .replace(/l’/gi, "")
     .replace(/l'/gi, "")
     .replace(/un’/gi, "")
     .replace(/\s+/gi, " ")
     .replace(/[\d,.'’:"”“»«?]|_/g, "")
-    .toLowerCase()
-    .split(" ");
+    .toLowerCase();
+};
+
+export const getWords = (highlight: string) => {
+  const cleaned = removePunctuation(highlight);
+  const split = cleaned.split(" ");
   return split;
 };
 
 export const getUniqueWords = (words: string[]) => {
-  return [... new Set(words)]
+  return [...new Set(words)];
 };
