d8e5c518237a40f98933950d5a8a7a8cf5dfc301
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-21 12:51:14 +0000

    Add modal at root

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { BooksView, ClippingsView, Upload, NotFound } from "./pages/index";
 import { LandingPage } from "./pages/LandingPage";
 import { Layout } from "./components/Layout";
+import { Modal } from "./components/Modal";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/books" element={<BooksView />} />
-        <Route path="/books/:bookName/*" element={<ClippingsView />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </Layout>
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/books" element={<BooksView />} />
+          <Route path="/books/:bookName/*" element={<ClippingsView />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+      <Modal />
+    </>
   );
 }
 
