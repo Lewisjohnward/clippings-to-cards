70db6b362232d39d0029ebe2fd5361475c3469e9
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-26 12:18:44 +0000

    Center text

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 export const NotFound = () => {
   return (
-    <div className="text-center [&>*]:pb-4">
+    <div className="flex flex-col justify-center text-center [&>*]:pb-4">
       <p className="text-4xl">404 not found</p>
-      <Link to="/" className="text-center ">
+      <Link to="/" className="text-center underline">
         Return home
       </Link>
     </div>
