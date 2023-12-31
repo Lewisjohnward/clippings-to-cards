b87db618bd9bf4755708c8e2b6d42ced7e941991
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-12-31 00:16:06 +0000

    Remove translate logic

--- src/components/Clipping.tsx
+++ src/components/Clipping.tsx
@@ -1,28 +1,18 @@
-import { Highlights, Translation } from "../types/Books";
+import { Highlights } from "../types/Books";
 import { IconButton } from "@material-tailwind/react";
-import { MdDelete, MdOutlineRemoveCircle } from "../misc/icons";
-import { useBookActions } from "../stores/useBookStore";
-import { useClipping } from "../hooks/useClipping";
-import { useState } from "react";
+import { MdDelete } from "../misc/icons";
 import clsx from "clsx";
+import { useClipping } from "@/hooks/useClipping";
 
 export default function Clipping({
   highlight,
   position,
-  translate,
 }: {
   highlight: Highlights;
   position: number;
-  translate: boolean;
 }) {
-  const {
-    date,
-    time,
-    handleTranslate,
-    handleToggleSelect,
-    handleDelete,
-    // translationButtonsVisible,
-  } = useClipping(highlight);
+  const { date, time, handleToggleSelect, handleDelete } =
+    useClipping(highlight);
 
   const background = (position + 1) % 2 == 0 ? "bg-yellow-50" : "bg-white";
 
@@ -40,28 +30,16 @@ export default function Clipping({
       <td className={clsx("text-xs text-center italic", background)}>
         {highlight.details.page}
       </td>
-      <td className={clsx("hidden md:table-cell text-xs text-center italic", background)}>
+      <td
+        className={clsx(
+          "hidden md:table-cell text-xs text-center italic",
+          background,
+        )}
+      >
         {highlight.details.words}
       </td>
       <td className={clsx("text-sm md:p-2 space-y-2 py-1", background)}>
-        {highlight.text.split(" ").map((word, i) => (
-          <p key={i} className="inline">
-            {translate ? (
-              <span
-                onClick={handleTranslate}
-                className="py-1 px-[2px] cursor-pointer hover:bg-blue-500 hover:bg-opacity-40"
-              >
-                {word}
-              </span>
-            ) : (
-              <span className="py-1 px-[2px]">{word} </span>
-            )}{" "}
-          </p>
-        ))}
-        <Translations
-          highlight={highlight}
-          // visible={translationButtonsVisible}
-        />
+        {highlight.text}
       </td>
       <td className={clsx("pr-4", background)}>
         <input
@@ -84,46 +62,3 @@ export default function Clipping({
     </>
   );
 }
-
-const Translations = ({ highlight }: { highlight: Highlights }) => {
-  if (highlight.translations.length == 0) return;
-  return (
-    <div>
-      {highlight.translations.map((translation) => {
-        return (
-          <div key={translation.id}>
-            <Tr highlight={highlight} translation={translation} />
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
-const Tr = ({
-  highlight,
-  translation,
-}: {
-  highlight: Highlights;
-  translation: Translation;
-}) => {
-  const [vis, setVis] = useState(false);
-  const { deleteTranslation } = useBookActions();
-  return (
-    <div
-      className="flex gap-2"
-      onMouseEnter={() => setVis(true)}
-      onMouseLeave={() => setVis(false)}
-    >
-      {vis && (
-        <button onClick={() => deleteTranslation(highlight, translation.id)}>
-          <MdOutlineRemoveCircle className="text-red-400 min-w-fit" />
-        </button>
-      )}
-      <p className="italic text-xs">
-        {translation.word} ({translation.type}) -{" "}
-        {translation.translation.join(", ")}
-      </p>
-    </div>
-  );
-};
