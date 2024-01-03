862d52efbde121f6859941a19d25ef93f99b0429
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-01-03 19:29:04 +0000

    Change background color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="flex justify-center bg-yellow-500 py-2">
+    <footer className="flex justify-center bg-yellow-400 py-2">
       <small>Clippings to Cards, 2023</small>
     </footer>
   );
