289596a17ee2c7d00d287f35c2d896c42a822c76
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-01-03 19:29:48 +0000

    Put 404 message center

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 export const NotFound = () => {
   return (
-    <div className="flex flex-col justify-center text-center [&>*]:pb-4">
-      <p className="text-4xl">404 not found</p>
-      <Link to="/" className="text-center underline">
-        Return home
-      </Link>
+    <div className="flex-grow flex flex-col justify-center text-center">
+      <div className="space-y-4">
+        <p className="text-4xl">404 not found</p>
+        <Link to="/" className="block text-center underline hover:opacity-40">
+          Return home
+        </Link>
+      </div>
     </div>
   );
 };
